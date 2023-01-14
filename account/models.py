784b8ac0501b15e62281c6b68ae897ea4c340593
adelia <adelia.ad03@gmail.com>
2023-01-14 21:10:27 +0600

    fixed

--- account/models.py
+++ account/models.py
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
     username = None
 
     USERNAME_FIELD =  'email'
