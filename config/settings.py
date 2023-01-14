78095b5bc4d7a3c8b319b2f69c35a15267317c9e
adelia <adelia.ad03@gmail.com>
2023-01-14 20:37:46 +0600

    fixed

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
-        'USER': config('DB_PASSWORD'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
